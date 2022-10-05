eaad3c38862f35fb1b9c739b3f3125ee76dcb7b1
cyberfreaq <cyberfreaq@gmx.net>
2022-10-05 16:32:17 +0200

    update docker install

--- kali-prep.zsh
+++ kali-prep.zsh
@@ -291,7 +291,8 @@ install_basic_packages () {
 }
 install_basic_packages
 
-
+# https://docs.docker.com/engine/install/debian/
+# https://www.kali.org/docs/containers/installing-docker-on-kali/
 install_docker () {
     CURRENT_TOOL="docker"
     TAGS=("base")
@@ -305,16 +306,13 @@ install_docker () {
             mkdir -p /etc/apt/keyrings
             apt-get remove docker docker-engine docker.io containerd runc
             apt-get -y install apt-transport-https ca-certificates curl gnupg-agent gnupg lsb-release software-properties-common
-            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
             echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+bullseye stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
             apt-get update
             apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
             systemctl enable docker
-            printf "${GREEN}[+] Installing docker-compose ...${NC}\n"
-            curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
-            chmod +x /usr/local/bin/docker-compose
         fi
     fi
 }
